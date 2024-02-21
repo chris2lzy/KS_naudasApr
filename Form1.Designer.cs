28946d184686198fa6f392a0b588a16427e1c85c
chris2lzy <kristofers2007arna@gmail.com>
2024-02-21 10:49:01 +0200

    nauda

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ienakumi = new System.Windows.Forms.Label();
+            this.izdevumi = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ire = new System.Windows.Forms.Label();
+            this.partika = new System.Windows.Forms.Label();
+            this.komunalie_maksajumi = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.budzets = new System.Windows.Forms.Label();
+            this.aprekinat = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ienakumi
+            // 
+            this.ienakumi.AutoSize = true;
+            this.ienakumi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.ienakumi.Location = new System.Drawing.Point(82, 36);
+            this.ienakumi.Name = "ienakumi";
+            this.ienakumi.Size = new System.Drawing.Size(64, 17);
+            this.ienakumi.TabIndex = 0;
+            this.ienakumi.Text = "ienākumi";
+            // 
+            // izdevumi
+            // 
+            this.izdevumi.AutoSize = true;
+            this.izdevumi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.izdevumi.Location = new System.Drawing.Point(29, 180);
+            this.izdevumi.Name = "izdevumi";
+            this.izdevumi.Size = new System.Drawing.Size(63, 17);
+            this.izdevumi.TabIndex = 1;
+            this.izdevumi.Text = "izdevumi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "alga";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "papildu ienākumi";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ire
+            // 
+            this.ire.AutoSize = true;
+            this.ire.Location = new System.Drawing.Point(38, 205);
+            this.ire.Name = "ire";
+            this.ire.Size = new System.Drawing.Size(19, 13);
+            this.ire.TabIndex = 6;
+            this.ire.Text = "īre";
+            // 
+            // partika
+            // 
+            this.partika.AutoSize = true;
+            this.partika.Location = new System.Drawing.Point(38, 243);
+            this.partika.Name = "partika";
+            this.partika.Size = new System.Drawing.Size(39, 13);
+            this.partika.TabIndex = 7;
+            this.partika.Text = "pārtika";
+            // 
+            // komunalie_maksajumi
+            // 
+            this.komunalie_maksajumi.AutoSize = true;
+            this.komunalie_maksajumi.Location = new System.Drawing.Point(38, 279);
+            this.komunalie_maksajumi.Name = "komunalie_maksajumi";
+            this.komunalie_maksajumi.Size = new System.Drawing.Size(105, 13);
+            this.komunalie_maksajumi.TabIndex = 8;
+            this.komunalie_maksajumi.Text = "komunālie maksājuki";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(164, 73);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 9;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(164, 108);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 10;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(164, 198);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 11;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(164, 236);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 12;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(164, 272);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown5.TabIndex = 13;
+            // 
+            // budzets
+            // 
+            this.budzets.AutoSize = true;
+            this.budzets.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.budzets.Location = new System.Drawing.Point(85, 328);
+            this.budzets.Name = "budzets";
+            this.budzets.Size = new System.Drawing.Size(58, 17);
+            this.budzets.TabIndex = 15;
+            this.budzets.Text = "budžets";
+            // 
+            // aprekinat
+            // 
+            this.aprekinat.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.aprekinat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F);
+            this.aprekinat.Location = new System.Drawing.Point(164, 328);
+            this.aprekinat.Name = "aprekinat";
+            this.aprekinat.Size = new System.Drawing.Size(75, 23);
+            this.aprekinat.TabIndex = 16;
+            this.aprekinat.Text = "aprēķināt";
+            this.aprekinat.UseVisualStyleBackColor = false;
+            this.aprekinat.Click += new System.EventHandler(this.aprekinat_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 357);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.aprekinat);
+            this.Controls.Add(this.budzets);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.komunalie_maksajumi);
+            this.Controls.Add(this.partika);
+            this.Controls.Add(this.ire);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.izdevumi);
+            this.Controls.Add(this.ienakumi);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ienakumi;
+        private System.Windows.Forms.Label izdevumi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ire;
+        private System.Windows.Forms.Label partika;
+        private System.Windows.Forms.Label komunalie_maksajumi;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Label budzets;
+        private System.Windows.Forms.Button aprekinat;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,49 @@ namespace KS_naudasApr
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void aprekinat_Click(object sender, EventArgs e)
+        {
+            int label1 = Convert.ToInt32(numericUpDown1.Value);
+
+            int label2 = Convert.ToInt32(numericUpDown2.Value);
+
+            int ire = Convert.ToInt32(numericUpDown3.Value);
+
+            int partika = Convert.ToInt32(numericUpDown4.Value);
+
+            int komunalie_maksajumi = Convert.ToInt32(numericUpDown5.Value);
+
+            int ienakumi = label1 + label2;
+
+            int izdevumi = ire + partika + komunalie_maksajumi;
+
+            int budzets = ienakumi - izdevumi;
+
+            textBox1.Text = budzets.ToString();
+
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
